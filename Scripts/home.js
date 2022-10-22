2733c79671e48274dcdd7537bd3c3f13b50d1bf7
hareesh1401 <97718635+hareesh1401@users.noreply.github.com>
2022-10-22 19:15:26 +0530

    Little Changes

---
+++ Scripts/home.js
@@ -0,0 +1,48 @@
+  // DATE 
+
+$( "#datepicker").datepicker();
+
+
+// SWIPER JS SPECIALISTS
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+  },
+});
+
+// OWL CAROSUAL TESTIMONIAL
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:2,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive : {
+      0 : {
+          items:1,
+          },
+      480 : {
+        items:2,
+          },
+      768 : {
+        items:2,
+      }
+  }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
